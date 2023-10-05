50fcd86bf1460a04851013298cd1db5f9c8cc928
FillippeLevenchuk <fial_93@hotmail.com>
2023-10-05 19:57:44 -0400

    COMMIT

---
+++ Combined_states.sql
@@ -0,0 +1,180 @@
+CREATE TABLE "Connecticut" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_Connecticut" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "Maine" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_Maine" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "Massachusetts" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_Massachusetts" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "NewHampshire" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_NewHampshire" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "NewJersey" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_NewJersey" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "NewYork" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_NewYork" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "Pennsylvania" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_Pennsylvania" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "PuertoRico" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_PuertoRico" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "RhodeIsland" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_RhodeIsland" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "Vermont" (
+    "ID" Varchar(30)   NOT NULL,
+    "bed" float   NOT NULL,
+    "bath" float   NOT NULL,
+    "acre_lot" float   NOT NULL,
+    "city" Varchar(30)   NOT NULL,
+    "state" Varchar(30)   NOT NULL,
+    "zip_code" float   NOT NULL,
+    "house_size" float   NOT NULL,
+    "price" float   NOT NULL,
+    "sold_previously" float   NOT NULL,
+    "latitude" float   NOT NULL,
+    "longitude" float   NOT NULL,
+    CONSTRAINT "pk_Vermont" PRIMARY KEY (
+        "ID"
+     )
+);
+
