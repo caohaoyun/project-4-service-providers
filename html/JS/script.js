0e7aa5c33ad44a5a04dda034f666ff541424714c
FillippeLevenchuk <fial_93@hotmail.com>
2023-10-10 22:02:25 -0400

    commit

--- html/JS/script.js
+++ html/JS/script.js
@@ -1,21 +1,14 @@
-const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3000;
-
-const db = new sqlite3.Database('new_database2.db');
+const db = new sqlite3.Database('../../Data/new_database2.db');
 
 app.use(express.static('public')); // Serve static files from a "public" folder
 
 app.get('/api/your-api-endpoint', (req, res) => {
     const selectedState = req.query.state;
 
-    if (!selectedState) {
-        res.status(400).json({ error: 'State not provided' });
-        return;
-    }
-
-    const tableName = selectedState.toLowerCase(); // Assuming table names are in lowercase
+    const tableName = selectedState.replace(/\s/g, ""); 
 
     const sql = `SELECT DISTINCT bed FROM "${tableName}"`;
 
