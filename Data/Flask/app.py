6503e9bc79c4355a7fb24ddb4190a7465dac4742
FillippeLevenchuk <fial_93@hotmail.com>
2023-10-12 18:10:20 -0400

    commit

--- Data/Flask/app.py
+++ Data/Flask/app.py
@@ -6,7 +6,11 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-database_path = os.path.abspath('Data\Flask\new_database2.db')
+# Get the directory where the script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Define the full path to the database file
+database_path = os.path.join(script_directory, 'new_database2.db')
 
 # Function to fetch data from a specific state's table
 def read_data_from_database(state):
@@ -19,12 +23,22 @@ def read_data_from_database(state):
 
 @app.route("/")
 def home():
-    """List all available API routes."""
-    return (
-        "Available Routes:<br/>"
-        "Data for a specific state: /api/v1.0/state/{state_name}<br/>"
-    )
+    return """ <h1><strong> Welcome to the East Coast House Listing API</strong> </h1>
+    <ul>
+    <li><h2>Choose one of the following APIs</h2></li>
+    <li><a href = "/api/v1.0/state/connecticut"> Connecticut's Listing  </a> <strong>/api/v1.0/state/connecticut</strong></li>
+    <li><a href = "/api/v1.0/state/maine"> Maine's Listing  </a> <strong>/api/v1.0/state/maine</strong></li>
+    <li><a href = "/api/v1.0/state/massachusetts"> Massachusetts's Listing  </a> <strong>/api/v1.0/state/massachusetts</strong></li>
+    <li><a href = "/api/v1.0/state/newhampshire"> New hampshire's Listing  </a> <strong>/api/v1.0/state/newhampshire</strong></li>
+    <li><a href = "/api/v1.0/state/newjersey"> New Jersey's Listing  </a> <strong>/api/v1.0/state/newjersey</strong></li>
+    <li><a href = "/api/v1.0/state/newyork"> New York's Listing  </a> <strong>/api/v1.0/state/newyork</strong></li>
+    <li><a href = "/api/v1.0/state/pennsylvania"> Pennsylvania's Listing  </a> <strong>/api/v1.0/state/pennsylvania</strong></li>
+    <li><a href = "/api/v1.0/state/puertorico"> Puerto Rico's Listing  </a> <strong>/api/v1.0/state/puertorico</strong></li>
+    <li><a href = "/api/v1.0/state/rhodeislands"> Rhode Islands's Listing  </a> <strong>/api/v1.0/state/rhodeislands</strong></li>
+    <li><a href = "/api/v1.0/state/vermont"> Vermont's Listing  </a> <strong>/api/v1.0/state/vermont</strong></li>
+    </ul>
 
+"""
 @app.route('/api/v1.0/state/<string:state_name>')
 def get_state_data(state_name):
     if state_name not in ["connecticut", "maine", "massachusetts", "newhampshire", "newjersey", "newyork", "pennsylvania", "puertorico", "rhodeisland", "vermont"]:
